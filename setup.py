cb4bb7396517a07d1b463ea2167c2a3eab677992
bendavid <joshbendavid@gmail.com>
2019-11-30 20:56:39 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       packages = ['pylgtv'],
       install_requires = ['websockets', 'asyncio'],
       zip_safe = True,
-      version = '0.1.9',
+      version = '0.2.0b1',
       description = 'Library to control webOS based LG Tv devices',
       author = 'Dennis Karpienski',
       author_email = 'dennis@karpienski.de',
