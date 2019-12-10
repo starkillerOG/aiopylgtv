fd6edeb54af9cb32b993f40fddcda08c7b5ec544
Bas Nijholt <basnijholt@gmail.com>
2019-12-10 16:21:01 +0100

    remove "asyncio" from requirements, it's in stdlib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="aiopylgtv",
     packages=["aiopylgtv"],
-    install_requires=["websockets", "asyncio", "numpy"],
+    install_requires=["websockets", "numpy"],
     zip_safe=True,
     version="0.2.5",
     description="Library to control webOS based LG Tv devices",
