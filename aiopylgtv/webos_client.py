6b494ba8af86687488d3398c7bf01e9cbd45f52a
Josh Bendavid <Josh.Bendavid@cern.ch>
2020-01-13 16:14:10 +0100

    make error handling more flexible to accommodate different behaviour of tv models (be tolerant of subscription requests which behave as normal requests)

--- aiopylgtv/webos_client.py
+++ aiopylgtv/webos_client.py
@@ -504,12 +504,7 @@ class WebOsClient:
         if payload is None:
             raise PyLGTVCmdException(f"Invalid request response {response}")
 
-        if cmd_type == "request":
-            returnValue = payload.get("returnValue")
-        elif cmd_type == "subscribe":
-            returnValue = payload.get("subscribed")
-        else:
-            returnValue = None
+        returnValue = payload.get("returnValue") or payload.get("subscribed")
 
         if returnValue is None:
             raise PyLGTVCmdException(f"Invalid request response {response}")
